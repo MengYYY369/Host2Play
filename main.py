2d9bf6752a9fac59f39d97899ef55de7817a280e
MY <3209617055@qq.com>
2026-08-08 01:39:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ except ImportError:
 
 # 配置区域
 RENEW_URLS = [
-    "https://host2play.gratis/server/renew?i=6666666-6666-6666-6666-66666666666",
+    "https://host2play.gratis/server/renew?i=1c3eb35f-942e-4420-9dd5-5dd942edd397",
     # 添加更多链接
 ]
 
